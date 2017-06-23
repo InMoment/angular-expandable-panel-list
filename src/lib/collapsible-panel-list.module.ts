1f9dce6e7728d54583a2f91b9646b0ec81d6c29d
Nathaniel Lord <nlord@inmoment.com>
2017-06-23 11:52:46 -0600

    Updates to ignore build files

--- src/lib/collapsible-panel-list.module.ts
+++ src/lib/collapsible-panel-list.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 
-import { MdExpansionPanel } from '@angular/material';
-
 import { CollapsiblePanelListComponent } from './collapsible-panel-list.component';
 import { CollapsiblePanelComponent } from './collapsible-panel.component';
 import { CollapsiblePanelListToggleComponent } from './collapsible-panel-list-toggle.component';
@@ -11,7 +9,7 @@ export { CollapsiblePanelComponent } from './collapsible-panel.component';
 export { CollapsiblePanelListToggleComponent } from './collapsible-panel-list-toggle.component';
 
 @NgModule({
-    imports: [ MdExpansionPanel ],
+    imports: [  ],
     declarations: [ CollapsiblePanelListComponent, CollapsiblePanelComponent, CollapsiblePanelListToggleComponent ],
     exports: [ CollapsiblePanelListComponent, CollapsiblePanelComponent, CollapsiblePanelListToggleComponent ]
 })
