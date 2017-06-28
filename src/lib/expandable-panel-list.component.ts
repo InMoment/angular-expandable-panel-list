fe822a38207798c81362e05bd7da4ea2ab993f0f
Nathaniel Lord <nathaniel.lord@gmail.com>
2017-06-28 16:19:47 -0600

    Recreated panel list without expansion panels

--- src/lib/expandable-panel-list.component.ts
+++ src/lib/expandable-panel-list.component.ts
@@ -1,12 +1,11 @@
-import { Component, ContentChildren, QueryList, Input } from '@angular/core';
+import { Component, ContentChildren, QueryList, Input, OnInit, ElementRef } from '@angular/core';
 
 import { ExpandablePanelComponent } from './expandable-panel.component';
 
 @Component({
     selector: 'expandable-panel-list',
     template: `
-    <div class="expandable-panel-container">
-        <expandable-list [items]="_panels"></expandable-list>
+    <div class="expandable-panel-container" [class.open]="open">
         <ng-content></ng-content>
     </div>
     `
@@ -16,5 +15,25 @@ export class ExpandablePanelListComponent {
     @Input() changeWidth = 768;
 
     @ContentChildren(ExpandablePanelComponent) _panels: QueryList<ExpandablePanelComponent>;
+
+    private open = false;
+    private myElement: ElementRef;
+
+    constructor(myElement: ElementRef) {
+        console.log(this._panels);
+    }
+
+    ngOnInit() {
+        console.log(this._panels);
+    }
+
+    ngAfterViewInit() {
+        console.log(this._panels);
+        this._panels.forEach(panel => {
+            panel.status.subscribe((event: boolean) => {
+                this.open = event;
+            });
+        });
+    }
 
 }
--- src/lib/expandable-panel.component.ts
+++ src/lib/expandable-panel.component.ts
@@ -1,15 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, ElementRef, OnInit, EventEmitter } from '@angular/core';
+import { DomSanitizer  } from '@angular/platform-browser';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
     selector: 'expandable-panel',
     template: `
-    <div class="expandable-panel" [class.open]="open">
-        <md-expansion-panel [expanded]="open" (closed)="closed()">
-            <md-expansion-panel-header>{{ title }}</md-expansion-panel-header>
+    <div class="expandable-panel" [class.open]="open" [style.top]="styleTop" [style.height]="styleHeight">
+        <div class="title ripple" (click)="toggle()">{{ title }}</div>
+        <div class="content">
             <ng-content></ng-content>
-        </md-expansion-panel>
+        </div>
     </div>
-    `
+    `,
+    animations: [
+        trigger('panelState',[
+            state('closed',style({
+                'top':'*'
+            })),
+            state('opened',style({
+                'top':'0px'
+            })),
+            transition('opened <=> closed', animate('500ms ease'))
+        ])
+    ]
 })
 export class ExpandablePanelComponent {
 
@@ -17,8 +36,51 @@ export class ExpandablePanelComponent {
 
     @Input() open: boolean;
 
-    closed() {
-        this.open = false;
+    @Output() status = new EventEmitter();
+
+    private state: string;
+
+    private myElement: ElementRef;
+    private top: string;
+    private height: string;
+    private styleHeight: string;
+    private styleTop: string;
+    private totalHeight: string;
+
+    constructor(myElement : ElementRef, private sanitizer: DomSanitizer) {
+        this.myElement = myElement;
+    }
+
+    setState(open: boolean) {
+        this.open = open;
+        if(this.open) {
+            this.state = 'opened';
+        } else {
+            this.state = 'closed';
+        }
+        this.status.emit(this.open);
+    }
+
+    ngAfterViewInit(){        
+        this.top = this.myElement.nativeElement.offsetTop + 'px';
+        this.height = this.myElement.nativeElement.innerHeight + 'px';
+        this.totalHeight = this.myElement.nativeElement.parentNode.innerHeight + 'px';
+        this.styleHeight = this.height;
+        this.styleTop = this.top;
+        this.setState(this.open);
+    }
+
+    toggle() {
+        if(this.open) {
+            this.styleTop = this.top;
+            this.styleHeight = this.height;
+            setTimeout(()=>this.setState(false),500);
+        } else {
+            this.styleTop = '0';
+            this.styleHeight = this.totalHeight;
+            this.setState(!this.open);
+        }
+        
     }
 
 }
